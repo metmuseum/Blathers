9b61d274427e1f9aa8ee2c62c11da487648b41b5
trouscaillon <andrew.r.allred@gmail.com>
2020-04-20 22:31:25 -0400

    Almost almost there

--- src/index.js
+++ src/index.js
@@ -6,15 +6,8 @@ import "./blathers.scss"
 var canvas = document.getElementById('imageToEncode'),
 context = canvas.getContext('2d');
 
-document.testImageToQr = function()
+document.imageToCanvas = function()
 {
-    var canvasQr = document.getElementById("qrCode");
-    var contextQr = canvasQr.getContext("2d");
-    var imageQr = new Image();
-    imageQr.onload = function() {
-        contextQr.drawImage(imageQr, 0, 0);
-    };
-
     var imageInput = new Image();
     imageInput.onload = function(){
         context.drawImage(imageInput, 0, 0);
@@ -22,9 +15,23 @@ document.testImageToQr = function()
         var drawingTool = new DrawingTool(canvas);
         const base64Image = ACNLQRGenerator(drawingTool);
 
-        console.log(drawingTool);                        
-        imageQr.src = base64Image;
+        base64Image.then(function(value) {
+            console.log(drawingTool);                        
+            document.canvasToQr(value);
+        });
     }
 
     imageInput.src = 'assets/images/download.png';
+}
+
+document.canvasToQr = function(data)
+{
+    var canvasQr = document.getElementById("qrCode");
+    var contextQr = canvasQr.getContext("2d");
+
+    var imageQr = new Image();
+    imageQr.onload = function() {
+        contextQr.drawImage(imageQr, 0, 0);
+    };
+    imageQr.src = data;
 }
