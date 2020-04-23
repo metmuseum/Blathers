6b4601de8fba71e7c18d1c3478495cea057e73a4
trouscaillon <andrew.r.allred@gmail.com>
2020-04-23 13:23:07 -0400

    No zoom on initial crop

--- src/cropTool.js
+++ src/cropTool.js
@@ -17,15 +17,21 @@ CropTool.init = (objectData) => {
 	crop.bind({
 		url: imgEl.src,
 		orientation: 1
+	}).then(function(){
+		crop.setZoom(0);
 	});
+	
+	
+	crop.setZoom(1);
 
 	crop.element.addEventListener('update', function(ev) {
-		const result = crop.result({type: `base64`, size: `viewport`});
-		result.then((value)=>{
-			generateQrCode(value, objectData.creator, objectData.title, objectData.island);
-		});
+		const result = crop.result({type: `base64`, size: `viewport`});		
 
+		result.then((value)=>{
+			generateQrCode(value, objectData.creator, objectData.title, objectData.island);			
+		});
 	});
+	
 
 }
 export default CropTool;
