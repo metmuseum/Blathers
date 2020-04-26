4854a4e1a0c41a2f9834cb3e2c91cc06f13529fa
Joseph Curley <hey.josephcurley@gmail.com>
2020-04-25 21:03:20 -0400

    Working on making a few sensible methods to make this a decent endpoint for others

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import ACNLQRGenerator from "./libs/ACNLQRGenerator";
 import DrawingTool from "./libs/DrawingTool";
 import CropTool from "./cropTool.js";
-import "./blathers.scss"
+import generateQrCode from "./qrGenerator.js";
 
-document.animalCrossingify =  function(objectData = {element: '.js-ac-image', creator: "The Met", island: "The Met"}) {
+import "./blathers.scss";
+
+document.animalCrossingify = function (
+	objectData = {
+		element: '.js-ac-image',
+ 		title: "Title",
+		creator: "Creator",
+		island: "Somewhere"}) {
 	CropTool.init(objectData);
+}
+
+document.animalCrossingifyWithoutCropping = function (
+	objectData = {
+		element: '.js-no-crop',
+ 		title: "Title",
+		creator: "Creator",
+		island: "Somewhere"
+	}) {
+
+	const imageSrc = document.querySelector(objectData.element).src;
+	generateQrCode(imageSrc, objectData.creator, objectData.title, objectData.island);
+
 }
--- src/qrGenerator.js
+++ src/qrGenerator.js
@@ -3,17 +3,17 @@ import DrawingTool from "./libs/DrawingTool";
 
 var draw = new DrawingTool();
 
-function imageToQr(imageData) 
+function imageToQr(imageData)
 {
     draw.getImagePaletteRgb(imageData);
 
     const promiseBase64 = ACNLQRGenerator(draw);
-    promiseBase64.then((data)=>{			
+    promiseBase64.then((data)=>{
         document.getElementById("qrCode").src = data;
     });
 }
 
-function generateQrCode(data, creator, title, town) 
+function generateQrCode(data, creator, title, town)
 {
     draw.creator = creator;
     draw.title = title;
@@ -26,9 +26,8 @@ function generateQrCode(data, creator, title, town)
 		canvasConvert.height = draw.height;
 		var contextConvert = canvasConvert.getContext("2d");
 		contextConvert.drawImage(image,0,0,draw.width,draw.height);
-        var imageData = contextConvert.getImageData(0, 0, draw.width, draw.height);	
-        
-        imageToQr(imageData);
+    var imageData = contextConvert.getImageData(0, 0, draw.width, draw.height);
+    imageToQr(imageData);
 	}
 	image.src = data;
 }
